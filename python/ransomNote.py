dc7584de89d2dff3d6c254ee5f31274c05df1418
Sunny Kim <tjdgmlkim19@gmail.com>
2022-07-04 20:19:43 -0500

    LeetCode #383 Ransom Note

---
+++ python/ransomNote.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        if len(ransomNote) > len(magazine): 
+            return False
+        
+        letters = collections.Counter(magazine)
+
+        for c in ransomNote:
+            if letters[c] <= 0:
+                return False
+            letters[c] -= 1
+        return True
