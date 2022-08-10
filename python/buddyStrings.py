1319294a4f243054141f1f037c7588bf4bcb4a58
Sunny Kim <tjdgmlkim19@gmail.com>
2022-08-10 15:27:41 -0500

    LeetCode #859 Buddy Strings

---
+++ python/buddyStrings.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal): 
+            return False
+        
+        if s == goal:
+            seen = set()
+            for a in s:
+                if a in seen:
+                    return True
+                seen.add(a)
+            return False
+
+        pairs = []
+        for a, b in zip(s, goal):
+            if a != b:
+                pairs.append((a, b))
+            if len(pairs) >= 3: 
+                return False
+        return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
